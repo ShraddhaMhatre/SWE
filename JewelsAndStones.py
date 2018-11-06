a075cb370da96543f8e68180da6ccee638b6faf6
ShraddhaMhatre <shraddha92mhatre@gmail.com>
2018-11-06 18:12:34 -0500

    Add files via upload

---
+++ JewelsAndStones.py
@@ -0,0 +1,14 @@
+#Amazon
+#Jewels & Stones: You want to know how many of the stones you have are also jewels. e.g. Input: J = "aA", S = "aAAbbbb" Output: 3
+class Solution(object):
+    count = 0
+    def numJewelsInStones(self, J, S):
+        for i in range(len(J)):
+            for j in range(len(S)):
+                if J[i] == S[j]:
+                    self.count += 1
+                else:
+                    continue
+        return self.count
+       
+        
